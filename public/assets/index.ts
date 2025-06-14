39531b2564aaf03b13b93924c5bc5af8d8914e1b
Hira Moueen <56905693+hirasheikh92@users.noreply.github.com>
2025-06-14 13:04:33 +0500

    Resolve merge conflict in assets/index.ts

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,11 +1,19 @@
-import logo from "./images/logo.png"
-import favicon from "./images/favicon.png"
-import panaverse1 from "./images/panaverse-1.png"
-import panaverse2 from "./images/panaverse-2.png"
-import pricing from "./images/pricing.png"
-import ecommerce from "./images/ecommerce.png"
-import currency from "./images/currency.png"
-import ochiDesign from "./images/ochi-design.png"
+import logo from "./images/logo.png";
+import favicon from "./images/favicon.png";
+import panaverse1 from "./images/panaverse-1.png";
+import panaverse2 from "./images/panaverse-2.png";
+import pricing from "./images/pricing.png";
+import ecommerce from "./images/ecommerce.png";
+import currency from "./images/currency.png";
+import ochiDesign from "./images/ochi-design.png";
 
-
-export {logo,favicon,panaverse1,panaverse2,pricing,ecommerce ,currency , ochiDesign}
+export {
+  logo,
+  favicon,
+  panaverse1,
+  panaverse2,
+  pricing,
+  ecommerce,
+  currency,
+  ochiDesign,
+};
